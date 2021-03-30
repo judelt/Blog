67c2f929282c8bb3b1d9ec350184d77fdd1e20c8
Judit Mendez Puig <mendez.puig@gmail.com>
2021-03-30 20:17:02 +0000

    Init

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+# A frequent practice is to place the standard CRUD actions in each
+# controller in the following order: index, show, new, edit, create, update
+# and destroy. 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def create
+    # @article = Article.new(params[:article])
+    # for security reasons
+    @article = Article.new(article_params)
+   
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+   
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+end
